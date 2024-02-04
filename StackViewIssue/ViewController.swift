637ca5e15bb7a50f29eed129d5a98608ac1f17c6
Srilatha Karancheti <slkarancheti@gmail.com>
2024-02-04 08:45:20 -0500

    Initial Commit

---
+++ StackViewIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewIssue
+//
+//  Created by Srilatha on 2024-02-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
